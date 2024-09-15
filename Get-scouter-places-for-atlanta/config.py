7f99dea3298979c38fa7a75c3be5dd8a4af5d403
unknown <dlovej009@gmail.com>
2024-09-14 21:18:36 -0400

    dj

--- Get-scouter-places-for-atlanta/config.py
+++ Get-scouter-places-for-atlanta/config.py
@@ -8,6 +8,6 @@ CITY_ID="85ab5e34-3d98-406f-a8c1-77df8ed68c2c"
 COUNTRY="USA"
 
 #PROXIES CONFIG UNITED STATES
-SEARCH_URL1='https://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&level=1&country=US'
-SEARCH_URL2='https://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve2=1&country=US'
-SEARCH_URL3='https://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve3=1&country=US'
+SEARCH_URL1='http://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&level=1&country=US'
+SEARCH_URL2='http://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve2=1&country=US'
+SEARCH_URL3='http://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve3=1&country=US'
--- Get-scouter-places-for-leeds/config.py
+++ Get-scouter-places-for-leeds/config.py
@@ -9,6 +9,6 @@ COUNTRY="UK"
 
 
 #PROXIES CONFIG UNITED STATES
-SEARCH_URL1='https://list.didsoft.com/get?email=tikuntechnologies@gmail.com&pass=bwnh68&pid=http1000&showcountry=no'
-SEARCH_URL2='https://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve2=1&country=US'
-SEARCH_URL3='https://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve3=1&country=US'
+SEARCH_URL1='http://list.didsoft.com/get?email=tikuntechnologies@gmail.com&pass=bwnh68&pid=http1000&showcountry=no'
+SEARCH_URL2='http://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve2=1&country=US'
+SEARCH_URL3='http://list.didsoft.com/get?email=rajeshkumardevapp@gmail.com&pass=zxamw8&pid=http1000&showcountry=no&leve3=1&country=US'
