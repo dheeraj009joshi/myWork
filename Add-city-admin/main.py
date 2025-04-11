5872a75be9cbb75ae38e8c866667853e77ad208f
Dheeraj Joshi <dheeraj@Dheerajs-MacBook-Air.local>
2025-04-11 21:13:51 +0530

    dj

--- Add-city-admin/main.py
+++ Add-city-admin/main.py
@@ -158,4 +158,4 @@ def stop_thread(type, thread_id):
         return jsonify({'success': False, 'error': 'Thread not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=5000)
